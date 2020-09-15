53c367e38a69148b72e368f8ce42a832221d4863
Michael Stramel <mstramel@coolfiresolutions.com>
2020-09-14 21:28:55 -0500

    adding more

--- README.md
+++ README.md
@@ -25,3 +25,29 @@ Your app is ready to be deployed!
 ### Q: What about Eject?
 
 No eject needed! Snowpack guarantees zero lock-in, and CSA strives for the same.
+
+## PWA Resources
+
+See https://web.dev/pwa-checklist/
+
+### App Manifest (`/public/manifest.webmanifest`)
+
+`manifest.webmanifest` (Sometimes also named `manifest.json`) provides metadata used when your web app is installed on a user's mobile device or desktop. See https://developers.google.com/web/fundamentals/web-app-manifest/
+
+### Robots.txt (`/public/robots.txt`)
+
+Web site owners use the `robots.txt` file to give instructions about their site to web robots; this is called The Robots Exclusion Protocol. See https://www.robotstxt.org/robotstxt.html
+
+### Web Vitals (`/src/reportWebVitals.ts`)
+
+Web Vitals is an initiative by Google to provide unified guidance for quality signals that are essential to delivering a great user experience on the web. See https://web.dev/vitals/
+
+
+### React Strict Mode
+
+### React Axe
+
+### Service Worker (`/src/service-worker.ts`)
+
+#### Registration Helpers (`/src/serviceWorkerRegistration.ts`)
+
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import type { ReportHandler } from 'web-vitals';
 import reportWebVitals from './reportWebVitals';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'
 import './index.css';
 
 async function start() {
@@ -26,7 +28,23 @@ if (import.meta.hot) {
   import.meta.hot.accept();
 }
 
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.unregister();
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+function sendToAnalytics(metric: ReportHandler) {
+  const body = JSON.stringify(metric);
+  // Use `navigator.sendBeacon()` if available, falling back to `fetch()`.
+  if (navigator.sendBeacon) {
+    navigator.sendBeacon('/analytics', body)
+  } else {
+    fetch('/analytics', {body, method: 'POST', keepalive: true});
+  }
+}
+
+reportWebVitals(console.log);
